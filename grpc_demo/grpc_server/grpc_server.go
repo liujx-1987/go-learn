d527905f077a68bc8daf93260e298f4bb9586ea0
liujinxin <liujinxin@yy.com>
2021-11-23 17:01:02 +0800

    add grpc_http

--- grpc_demo/grpc_server/grpc_server.go
+++ grpc_demo/grpc_server/grpc_server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
 	"CoolCar/service"
 
@@ -13,6 +14,12 @@ import (
 )
 
 func main() {
+	// grpc_main()
+	grpc_http()
+
+}
+
+func grpc_main() {
 	// 1. 引用证书
 	tls, err := credentials.NewServerTLSFromFile("./keys/cert.pem", "./keys/key.pem")
 	if err != nil {
@@ -34,5 +41,37 @@ func main() {
 	fmt.Println("start grpc Server")
 	// 5. 运行rpcServer，传入listener
 	_ = rpcServer.Serve(listener)
+}
+
+func grpc_http() {
+	// 1. 引用证书
+	tls, err := credentials.NewServerTLSFromFile("./keys/cert.pem", "./keys/key.pem")
+	if err != nil {
+		log.Fatal("服务端获取证书失败: ", err)
+	}
+
+	// 2. new一个grpc的server，并且加入证书
+	rpcServer := grpc.NewServer(grpc.Creds(tls))
+
+	// 3. 将刚刚我们新建的ProdService注册进去
+	service.RegisterProdServiceServer(rpcServer, new(service.ProductService))
+
+	// 4. 新建一个路由，并传入rpcServer
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println(request)
+		rpcServer.ServeHTTP(writer, request)
+	})
+
+	// 5. 定义httpServer，监听8082
+	httpServer := http.Server{
+		Addr:    ":8082",
+		Handler: mux,
+	}
+
+	fmt.Println("start grpc Server")
+
+	// 6. 以https形式监听httpServer
+	httpServer.ListenAndServeTLS("./keys/cert.pem", "./keys/key.pem")
 
 }
